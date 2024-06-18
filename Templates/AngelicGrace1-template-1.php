5116d2ac442a898fd4eef335ccab56a9ffa7a530
DarkSocietyDs <160298650+DarkSocietyDs@users.noreply.github.com>
2024-06-18 17:31:29 -0600

    AngelicGrace1-template-1.php

---
+++ Templates/AngelicGrace1-template-1.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Imprimir Bautizo</title>
+
+	<?php
+		include('../../library/bootstrap_v5_3.php');
+		include('../../library/jquery_v351.php');
+	?>
+</head>
+<body>
+
+	<h1>.$ParroquiaBautismo.</h1>
+	
+</body>
+</html>
